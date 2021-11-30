3913bcde7f48a53a4869420e9a52b2e2215b19de
Marcos <marcosverdile@gmail.com>
2021-11-29 21:48:09 -0300

    modifique el codigo Ãpara que sea una funcion y almacene los datos en un array lo que facilitaria su añadido al codigo final

--- verdile.php
+++ verdile.php
@@ -1,6 +1,6 @@
 <?php
 
-$coleccionJuegos = [
+$arrayDeEjemplo = [
 ["jugadorCruz" => "a" , "jugadorCirculo" => "b" , "puntosCruz" => 2, "puntosCirculo" => 0],
 ["jugadorCruz" => "a" , "jugadorCirculo" => "b" , "puntosCruz" => 2, "puntosCirculo" => 0],
 ["jugadorCruz" => "a" , "jugadorCirculo" => "b" , "puntosCruz" => 2, "puntosCirculo" => 0],
@@ -11,98 +11,112 @@ $coleccionJuegos = [
 ];
 
 
-$n = count ($coleccionJuegos);
-$i = 0;
-$flag = true;
-$cruzWin = 0;
-$circuloWin= 0;
-$cruzLose = 0;
-$circuloLose= 0;
-$cruzDraw = 0;
-$puntosTotal = 0;
-$puntosCirculo = 0;
+// Punto 5)
 
-echo "menu de opciones 1 , 2 , 3 , 4 , 5";
-echo "\n";
-echo "que submenu desea solicitar?";
-echo "\n";
-$menuDeseado = trim(fgets(STDIN));
-if ($menuDeseado == "5")
- { 
+function resumenJugador($coleccionJuegos) { 
+
+    $n = count ($coleccionJuegos);
+    $i = 0;
+    $flag = true;
+    $puntosGanar = 0;
+    $puntosPerder = 0;
+    $puntosEmpatar = 0;
+    $puntosTotal = 0;
+
    echo "ingrese nombre de jugador \n";
    $nombreJugador = trim(fgets(STDIN));
-   //$nombreJugador = strtoupper ($nombreJugador);
-   while ($i < $n and $flag == true ) {
-       if ($nombreJugador == $coleccionJuegos[$i]["jugadorCruz"]) {
-          if ($coleccionJuegos[$i]["puntosCruz"] > $coleccionJuegos[$i]["puntosCirculo"]) {
-          
-           $cruzWin++;
-           $puntosTotal = $puntosTotal + $coleccionJuegos[$i]["puntosCruz"];
-           $i++;
-           
-           
-       }   
-       elseif ($nombreJugador == $coleccionJuegos[$i]["jugadorCruz"]) {
-        if ($coleccionJuegos[$i]["puntosCruz"] == $coleccionJuegos[$i]["puntosCirculo"]) {
-          
-          $cruzDraw++;
+
+   for ($i = 0; $i < $n; $i++) {
+
+    if ($nombreJugador == $coleccionJuegos[$i]["jugadorCruz"] || $nombreJugador == $coleccionJuegos[$i]["jugadorCruz"]) {
+        
+   
+      if ($nombreJugador == $coleccionJuegos[$i]["jugadorCruz"]) {
+
+         if ($coleccionJuegos[$i]["puntosCruz"] > $coleccionJuegos[$i]["puntosCirculo"]) {
+        
+          $puntosGanar++;
           $puntosTotal = $puntosTotal + $coleccionJuegos[$i]["puntosCruz"];
           $i++;
           
-         
-        }
-       else {
-                $cruzLose++;
-                $i++;
-              }    
+          
+        } elseif ($nombreJugador == $coleccionJuegos[$i]["jugadorCruz"]) {
 
-   } 
-   }
-   elseif ($nombreJugador == $coleccionJuegos[$i]["jugadorCirculo"]) {
+          if ($coleccionJuegos[$i]["puntosCruz"] == $coleccionJuegos[$i]["puntosCirculo"]) {
+           
+            $puntosEmpatar++;
+            $puntosTotal = $puntosTotal + $coleccionJuegos[$i]["puntosCruz"];
+            $i++;
+           
+           
+          } else {
+
+              $puntosPerder++;
+              $i++;
+              
+            }    
+              
+        } 
+      } elseif ($nombreJugador == $coleccionJuegos[$i]["jugadorCirculo"]) {
+
         if ($coleccionJuegos[$i]["puntosCruz"] < $coleccionJuegos[$i]["puntosCirculo"]) {
         
-           $cruzWin++;
+           $puntosGanar++;
            $puntosTotal = $puntosTotal + $coleccionJuegos[$i]["puntosCirculo"];
            $i++;
-           
-       
-         }
-         elseif ($nombreJugador == $coleccionJuegos[$i]["jugadorCirculo"]) {
+        
+        } elseif ($nombreJugador == $coleccionJuegos[$i]["jugadorCirculo"]) {
+
           if ($coleccionJuegos[$i]["puntosCruz"] == $coleccionJuegos[$i]["puntosCirculo"]) {
-            
-            $cruzDraw++;
+            $puntosEmpatar++;
             $puntosTotal = $puntosTotal + $coleccionJuegos[$i]["puntosCirculo"];
             $i++;
             
-            }
-         else {
-             $cruzLose++;
+          } else {
+            
+             $puntosPerder++;
              $i++;
-             
-         }
-         }
-   
 
+            }
+
+        }
         
-   }
-   
-   echo "Jugador ", $nombreJugador ," " ,"gano ", $cruzWin." "."juegos\n";
-   echo "Jugador ", $nombreJugador ," " ,"perdio ", $cruzLose." "."juegos\n";
-   echo "Jugador ", $nombreJugador ," " ,"empato ", $cruzDraw." "."juegos\n";
-   echo "Jugador ", $nombreJugador ," " ,"puntos ", $puntosTotal." "."puntos\n";
-   }
+      }
 
+    }
 
+  }
 
+  $nombreJugador = array(
+    "nombre" => $nombreJugador,
+    "juegosGanados" => $puntosGanar,
+    "juegosPerdidos" => $puntosPerder,
+    "juegosEmpatados" => $puntosEmpatar,
+    "puntosAcumulados" => $puntosTotal,
+  );
 
-
+  return $nombreJugador;
 }
-
-else 
-{echo "cagaste mi loco, aun no implementamos esa funcion jeje";}
-
-
-    
                 
 
 
+//var_dump(resumenJugador($arrayDeEjemplo));
+
+
+// ------------------------------------------
+// Punto 6)
+
+function ordenadoPorO($coleccionDeJuegos) {
+
+  function cmp($a, $b) {
+    if($a['jugadorCirculo'] == $b['jugadorCruz']) {
+        return 0;
+    }
+    return ($a['jugadorCirculo'] > $b['jugadorCruz']) ? -1 : 1;
+}
+
+
+  print_r(uasort($coleccionDeJuegos, 'cmp'));
+}
+
+ordenadoPorO($arrayDeEjemplo);
