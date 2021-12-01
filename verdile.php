9656eeba74d55f88469cf6dd991f9612f40c03a3
Marcos <marcosverdile@gmail.com>
2021-11-30 21:22:52 -0300

    codigo 5 comentado y codigo 6 casi listo

--- verdile.php
+++ verdile.php
@@ -1,20 +1,22 @@
 <?php
-
+// cargo 7 juegos de ejemplo en el array para poder usar en la funcion
 $arrayDeEjemplo = [
 ["jugadorCruz" => "a" , "jugadorCirculo" => "b" , "puntosCruz" => 2, "puntosCirculo" => 0],
 ["jugadorCruz" => "a" , "jugadorCirculo" => "b" , "puntosCruz" => 2, "puntosCirculo" => 0],
 ["jugadorCruz" => "a" , "jugadorCirculo" => "b" , "puntosCruz" => 2, "puntosCirculo" => 0],
 ["jugadorCruz" => "a" , "jugadorCirculo" => "b" , "puntosCruz" => 0, "puntosCirculo" => 2],
 ["jugadorCruz" => "a" , "jugadorCirculo" => "b" , "puntosCruz" => 2, "puntosCirculo" => 1],
-["jugadorCruz" => "a" , "jugadorCirculo" => "b" , "puntosCruz" => 1, "puntosCirculo" => 1],
-["jugadorCruz" => "a" , "jugadorCirculo" => "b" , "puntosCruz" => 1, "puntosCirculo" => 1],
+["jugadorCruz" => "b" , "jugadorCirculo" => "a" , "puntosCruz" => 1, "puntosCirculo" => 1],
+["jugadorCruz" => "b" , "jugadorCirculo" => "a" , "puntosCruz" => 1, "puntosCirculo" => 1],
 ];
 
 
 // Punto 5)
 
+//empieza la función
+//@param array $coleccionJuegos
 function resumenJugador($coleccionJuegos) { 
-
+//inicializo las variables
     $n = count ($coleccionJuegos);
     $i = 0;
     $flag = true;
@@ -22,12 +24,13 @@ function resumenJugador($coleccionJuegos) {
     $puntosPerder = 0;
     $puntosEmpatar = 0;
     $puntosTotal = 0;
-
+  
    echo "ingrese nombre de jugador \n";
    $nombreJugador = trim(fgets(STDIN));
-
+  // estructura repetitiva que se ejecuta cuando la cantidad de juegos sea mayor a 0
    for ($i = 0; $i < $n; $i++) {
-
+  //estructura del tipo si que evalua coincidencias en el nombre del jugador con el registro de juegos y asigna un contador por cada resultado posible
+  //se les agrega un contador para aumentar el valor con el que se comparan la cantidad de juegos y usarlo de stop
     if ($nombreJugador == $coleccionJuegos[$i]["jugadorCruz"] || $nombreJugador == $coleccionJuegos[$i]["jugadorCruz"]) {
         
    
@@ -86,7 +89,7 @@ function resumenJugador($coleccionJuegos) {
     }
 
   }
-
+  // se crea un array para almacenar el nombre y el registro de juegos del jugador
   $nombreJugador = array(
     "nombre" => $nombreJugador,
     "juegosGanados" => $puntosGanar,
@@ -94,7 +97,7 @@ function resumenJugador($coleccionJuegos) {
     "juegosEmpatados" => $puntosEmpatar,
     "puntosAcumulados" => $puntosTotal,
   );
-
+  //retorno el nombre del jugador en cuestion.
   return $nombreJugador;
 }
                 
@@ -115,8 +118,8 @@ function ordenadoPorO($coleccionDeJuegos) {
     return ($a['jugadorCirculo'] > $b['jugadorCruz']) ? -1 : 1;
 }
 
-
-  print_r(uasort($coleccionDeJuegos, 'cmp'));
+  uasort($coleccionDeJuegos, 'cmp');
+  print_r($coleccionDeJuegos);
 }
 
 ordenadoPorO($arrayDeEjemplo);
